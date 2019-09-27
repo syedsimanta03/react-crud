e0fb2f8963a2e774d0997fb77b9cdca198556d46
syedsimanta03 <syed.simanta06@gmail.com>
2019-09-27 17:03:11 +0600

    Updates

---
+++ precache-manifest.599dc7088164e33b7916eb083c35a1bc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b87a457b572495bcb8fc1e218f357942",
+    "url": "/react-crud/index.html"
+  },
+  {
+    "revision": "c7b751cfc0e07868eedf",
+    "url": "/react-crud/static/css/main.cb9a33a3.chunk.css"
+  },
+  {
+    "revision": "5996b2a742fc17c2433d",
+    "url": "/react-crud/static/js/2.e88548fa.chunk.js"
+  },
+  {
+    "revision": "c7b751cfc0e07868eedf",
+    "url": "/react-crud/static/js/main.23c02f73.chunk.js"
+  },
+  {
+    "revision": "fcbde5cd661a16bfd9f9",
+    "url": "/react-crud/static/js/runtime-main.043e4c30.js"
+  }
+]);
